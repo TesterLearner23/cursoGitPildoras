e36bfba1831c7fa2ec8835e88c7be8e4fd33de56
iceman <icemandld23@gmail.com>
2025-05-15 14:05:01 -0600

    Se agregan dos colores más al cambio del título con otro botón

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -2,5 +2,10 @@ function cambiarColor(){
    let element = document.getElementById("main-title");
    state = element.getAttribute("style")==="color:orange";
    state ? element.setAttribute("style","color:green"):element.setAttribute("style","color:orange");
-   state = !state;
+}
+
+function cambiarColor2(){
+   let element = document.getElementById("main-title");
+   state = element.getAttribute("style")==="color:blue";
+   state ? element.setAttribute("style","color:red"):element.setAttribute("style","color:blue");
 }
