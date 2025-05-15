33cacc37eb51446b652f3b4723bb66cb9a2e1e54
iceman <icemandld23@gmail.com>
2025-05-15 14:30:53 -0600

    Se agrega cambio de color a título principal cuando curso pasa encima

---
+++ js/funcionalidades.js
@@ -0,0 +1,9 @@
+function overSubtitle(){
+   let element = document.getElementById("main-subtitle");
+   element.setAttribute("style","color:cyan");
+}
+
+function leaveSubtitle(){
+   let element = document.getElementById("main-subtitle");
+   element.setAttribute("style","color:antiquewhite");
+}
